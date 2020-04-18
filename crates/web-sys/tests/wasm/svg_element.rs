6ad9340c68d4ddfb7a62f85fe1469446f7fd29ab
liquidblock <liquidblock@users.noreply.github.com>
2020-04-18 23:01:47 +0200

    Add different class attribute tests for svg elements and split Element::set_class_name and Element::class_name into 2 test cases

--- crates/web-sys/tests/wasm/svg_element.rs
+++ crates/web-sys/tests/wasm/svg_element.rs
@@ -9,7 +9,43 @@ extern "C" {
 }
 
 #[wasm_bindgen_test]
-fn svg_element() {
+fn element_class_attribute() {
+    let svg = new_svg();
+
+    // test class list
+    assert_eq!(
+        svg.get_attribute("class"),
+        None,
+        "Shouldn't have the class attribute"
+    );
+    svg.set_attribute("class", "test");
+    assert_eq!(
+        svg.get_attribute("class"),
+        Some(String::from("test")),
+        "Should have the class attribute set"
+    );
+}
+
+#[wasm_bindgen_test]
+fn element_class_list() {
+    let svg = new_svg();
+
+    // test class list
+    assert_eq!(
+        svg.class_list().value(),
+        "",
+        "Shouldn't have a class_list value"
+    );
+    svg.class_list().set_value("test");
+    assert_eq!(
+        svg.class_list().value(),
+        "test",
+        "Should have a class_list value"
+    );
+}
+
+#[wasm_bindgen_test]
+fn svgelement_class_name() {
     let svg = new_svg();
 
     // test class_name
@@ -24,11 +60,30 @@ fn svg_element() {
         "test",
         "Should have a class_name"
     );
+}
 
+#[wasm_bindgen_test]
+fn element_set_class_name() {
+    // test set_class_name on Element
+    let svg = new_svg();
+    let element: &Element = &svg;
+    element.set_class_name("test"); // using Element::set_class_name instead of SvgElement::class_name
+    assert_eq!(
+        element.get_attribute("class"), // test using the class attribute
+        Some(String::from("test")),
+        "Should have a class_name"
+    );
+}
+
+#[wasm_bindgen_test]
+fn element_get_class_name() {
     // test class_name on Element
     let svg = new_svg();
     let element: &Element = &svg;
-    assert_eq!(element.class_name(), "", "Shouldn't have a class_name");
-    element.set_class_name("test");
-    assert_eq!(element.class_name(), "test", "Should have a class_name");
+    svg.set_attribute("class", "test"); // test using the class attribute
+    assert_eq!(
+        element.class_name(), // using Element::class_name instead of SvgElement::class_name
+        "test",
+        "Should have a class_name"
+    );
 }
